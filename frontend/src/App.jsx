a11530f6d64b0b5bf2660077d033bb51838fb9b9
Chandan Dhamande <chandan@Chandans-Macbook-Pro.local>
2025-10-25 18:31:37 -0700

    Integrated AWS maps and location feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import OdysseyLanding from "./components/OdysseyLanding";
 import SignIn from "./components/Signin";
+import TripMap from "./components/TripMap";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<OdysseyLanding />} />
           <Route path="/login" element={<SignIn />} />
+          <Route path="/trip" element={<TripMap/>} />
         </Routes>
       </div>
     </Router>
